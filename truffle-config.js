6287690d1de1353f1e1edbc7d22f527ed463ec87
jonsax6 <jdc.saxjunky@gmail.com>
2020-12-10 01:05:14 -0500

    initial commit - folder structure

---
+++ truffle-config.js
@@ -0,0 +1,44 @@
+
+
+const HDWalletProvider = require('@truffle/hdwallet-provider'); 
+require("dotenv").config(); // imports all environmental variables
+
+module.exports = {
+  networks: {
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+     gasPrice:10e9
+    }, 
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, `https://ropsten.infura.io/v3/${process.env.INFURA_KEY}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.5.12",    // Fetch exact version from solc-bin (default: truffle's version)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: true,
+         runs: 200
+       },
+      }
+    }
+  },
+
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY
+  },
+
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+}
